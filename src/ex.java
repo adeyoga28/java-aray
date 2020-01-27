431dba24ca24c7b41660c8bae0cca250f1836110
ade yoga <57425861+adeyoga28@users.noreply.github.com>
2020-01-27 12:30:25 +0700

    Add files via upload

---
+++ src/ex.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author windbit10
+ */
+
+import java.util.Scanner; // import the Scanner class 
+
+class MyClass {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    
+    // Enter username and press Enter
+    System.out.println("Enter username"); 
+    userName = myObj.nextLine();   
+       
+    System.out.println("Username is: " + userName);        
+  }
+}
+
